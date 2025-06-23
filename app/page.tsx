ce42d2840a995103d1b44065514584ce70049f8a
akiko <lunlun.smiling@gmail.com>
2025-06-23 20:46:32 +0900

    Add my Inspiration

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <p className="text-lg drop-shadow">
             分子栄養学に基づく細胞から支えるスナック
           </p>
-          <h2 className="bg-green-600">Almu</h2>
+          <h2 className="text-green text-lg drop-shadow">Almu</h2>
           <div className="flex flex-col sm:flex-row gap-4 justify-center mt-4">
             <Link href="/products" className="text-gray-700 hover:underline">
               <button className="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-3 rounded">
